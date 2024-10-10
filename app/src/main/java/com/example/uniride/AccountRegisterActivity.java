a0f9e6e3272c92ab1d94c7865f92eb972f43c4ea
Luke Aniago <luke_marion_aniago@dlsu.edu.ph>
2024-10-10 22:01:33 +0800

    Register Drafted

--- app/src/main/java/com/example/uniride/AccountRegisterActivity.java
+++ app/src/main/java/com/example/uniride/AccountRegisterActivity.java
@@ -1,9 +1,6 @@
 package com.example.uniride;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,27 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class AccountLoginActivity extends AppCompatActivity {
-
-    TextView signup;
+public class AccountRegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_account_login);
+        setContentView(R.layout.activity_account_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        signup = (TextView) findViewById(R.id.textViewSignUp);
     }
-
-    public void signup(View view) {
-        Intent intent = new Intent(this, AccountRegisterActivity.class);
-        startActivity(intent);
-    }
-
 }
