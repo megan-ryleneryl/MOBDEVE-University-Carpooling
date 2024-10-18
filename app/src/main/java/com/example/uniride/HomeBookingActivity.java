1627f88e0a633cd0aed7f94db1703208d0ed0135
Harold <harold_mojica@dlsu.edu.ph>
2024-10-19 06:05:37 +0800

    Connected buttons to pages

--- app/src/main/java/com/example/uniride/HomeBookingActivity.java
+++ app/src/main/java/com/example/uniride/HomeBookingActivity.java
@@ -1,6 +1,14 @@
 package com.example.uniride;
 
+import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +16,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 public class HomeBookingActivity extends AppCompatActivity {
+
+    TextView titleText;
+    String bookingType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_booking);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        titleText = findViewById(R.id.titleText);
+
+        Intent i = getIntent();
+        bookingType = i.getStringExtra("bookingTypePassed");
+
+        //if (bookingType == "scheduled")
     }
 }
