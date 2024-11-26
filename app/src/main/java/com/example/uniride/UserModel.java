9dd30c22d48d885ca8501c9e9a604bc4a3d9f84b
Harold <harold_mojica@dlsu.edu.ph>
2024-11-26 10:38:24 +0800

    Fixed crash in home screen due to balance

--- app/src/main/java/com/example/uniride/UserModel.java
+++ app/src/main/java/com/example/uniride/UserModel.java
@@ -156,7 +156,11 @@ public class UserModel implements Serializable {
             user.carID = ((Long) map.get("carID")).intValue();
         }
         if (map.get("balance") != null) {
-            user.balance = ((Long) map.get("balance")).doubleValue();
+            if (map.get("balance") instanceof Long) {
+                user.balance = ((Long) map.get("balance")).doubleValue();
+            } else {
+                user.balance = (Double) map.get("balance");
+            }
         }
         // Add these lines to load license details
         user.licenseNumber = (String) map.get("licenseNumber");
